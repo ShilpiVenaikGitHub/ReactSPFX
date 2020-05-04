3a64eb913321b07636001a4340385e3df318a98e
Shilpi Venaik <64773202+ShilpiVenaikGitHub@users.noreply.github.com>
2020-05-04 19:41:23 +0530

    Initial commit

---
+++ src/webparts/reactspfxhelloworld/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IReactspfxhelloworldWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'ReactspfxhelloworldWebPartStrings' {
+  const strings: IReactspfxhelloworldWebPartStrings;
+  export = strings;
+}
